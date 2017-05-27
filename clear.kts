d5b18b6bd5487b96bdc9aae3e110cc0a677458de
kingty <398036899@qq.com>
2017-05-27 13:56:05 +0800

    Update clear.kts

--- clear.kts
+++ clear.kts
@@ -1,16 +1,16 @@
 import java.io.File
 
 val pattern = """@mipmap\/[a-zA-Z0-9_]+[^a-zA-Z0-9_]|R\.mipmap\.[A-Za-z0-9_]+[^a-zA-Z0-9_]""".toPattern()
-val resDirs = sequenceOf("-hdpi", "-mdpi", "-xhdpi", "-xxhdpi", "-xxxhdpi")
+val resDirs = sequenceOf("-hdpi", "-mdpi", "-xhdpi", "-xxhdpi", "-xxxhdpi").map { s -> "app/src/main/res/mipmap$s" }.asSequence()
 val drawables = sequenceOf("png", "jpg", "xml")
 val codes = sequenceOf("kt", "java", "xml")
 val projects = sequenceOf("app")
 
-fun getname(f: File): String {
+fun getFileName(f: File): String {
     return f.name.replace(".9.png", "").replace(".png", "").replace(".jpg", "").replace(".xml", "")
 }
 
-fun fileString(fn: File) = fn.readLines().filter { l -> !l.trim().startsWith("//") }.asSequence().joinToString("\n")
+fun fileToString(fn: File) = fn.readLines().filter { l -> !l.trim().startsWith("//") }.asSequence().joinToString("\n")
 
 fun allFiles(f: File, extensions: Sequence<String>): Sequence<File> {
     val files = ArrayList<File>()
@@ -22,38 +22,33 @@ fun allFiles(f: File, extensions: Sequence<String>): Sequence<File> {
     return files.asSequence()
 }
 
-fun reses(): Sequence<File> {
-    return resDirs.map { d -> "app/src/main/res/mipmap$d" }.map { str -> allFiles(File(str), drawables) }.flatten()
-}
+fun reses(): Sequence<File> = resDirs.map { str -> allFiles(File(str), drawables) }.flatten()
 
-fun all_names() = reses().map { file -> getname(file) }.toSet()
+fun allNames() = reses().map { file -> getFileName(file) }.toSet()
 
-fun used_names(): Set<String> {
-    return projects.map { p ->
-        allFiles(File(p + "/src"), codes).map { f -> fileString(f) }.map { str ->
-            val names = ArrayList<String>()
-            val m = pattern.matcher(str)
-            while (m.find()) {
-                names.add(m.toMatchResult().group().dropLast(1).replace("@mipmap/", "").replace("R.mipmap.", ""))
-            }
-            names.asSequence()
-        }.flatten()
-    }.flatten().toSet()
-}
+val usedNames = projects.map { p ->
+    allFiles(File(p + "/src"), codes).map { f -> fileToString(f) }.map { str ->
+        val names = ArrayList<String>()
+        val m = pattern.matcher(str)
+        while (m.find()) {
+            names.add(m.toMatchResult().group().dropLast(1).replace("@mipmap/", "").replace("R.mipmap.", ""))
+        }
+        names.asSequence()
+    }.flatten()
+}.flatten().toSet()
 
-val unused_names = reses().map { file -> getname(file) }.filter { d -> !used_names().contains(d) }.toSet()
-
+val unusedNames = reses().map { file -> getFileName(file) }.filter { d -> !usedNames.contains(d) }.toSet()
 
 fun cleanDir(f: File) {
     if (f.isDirectory) {
         f.listFiles().forEach { f -> cleanDir(f) }
     } else {
-        if (unused_names.map { s -> getname(f) == s }.toSet().contains(true)) {
+        if (unusedNames.map { s -> getFileName(f) == s }.toSet().contains(true)) {
             f.delete()
         }
     }
 }
 
-fun clean() = resDirs.map { d -> "app/src/main/res/mipmap$d" }.map { s -> File(s) }.forEach { f -> cleanDir(f) }
+fun clean() = resDirs.map { s -> File(s) }.forEach { f -> cleanDir(f) }
 
 clean()
